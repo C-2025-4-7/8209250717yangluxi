5690086f4c088247cc79ab631004f4123a3e6dfa
lucy-155 <2390645966@qq.com>
2025-11-21 09:06:40 +0800

    Add files via upload

--- 250717-杨鲁玺/1.5.cpp
+++ 250717-杨鲁玺/1.5.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
-int main() {
-	double F;
-	double C;
-	cout << "请输入华氏温度:" ;
+int main()
+{
+	float F, C;
+	cout << "请输入华氏温度：" ;
 	cin >> F;
-	cout << "其对应的摄氏度为:" << (F - 32) / 1.8 << "°C" << endl;
+	cout <<fixed<<setprecision(2);
+	cout << "摄氏温度为：" << (C = 5 * (F - 32) / 9 )<< endl;
 	return 0;
 
 }
